da0de37f5faadf195f519cb88b07d6dfc7264477
Peter ngugi <peter.ngugi.r@gmail.com>
2026-08-13 15:03:28 +0300

    Organizer page and blog page

---
+++ src/Organizerpage/Info.jsx
@@ -0,0 +1,20 @@
+import "./Info.css";
+
+function Info() {
+  return (
+    <section className="info-hero">
+      <span className="info-badge">FOR PROMOTERS</span>
+      <h1 className="info-title">
+        Bring Your Event to
+        <br />
+        Thousands of Fans
+      </h1>
+      <p className="info-description">
+        Partner with TicketYetu and get your event in front of a passionate
+        audience. Follow the steps below to list your event on our platform.
+      </p>
+    </section>
+  );
+}
+
+export default Info;
